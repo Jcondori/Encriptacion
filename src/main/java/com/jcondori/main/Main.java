959715bc65daedc2fd943a5a5e167c45331f22d9
Jose Luis Condori Jara <jcondori@vallegrande.edu.pe>
2018-11-26 19:26:28 -0500

    Implementacion de libreria ascii

--- src/main/java/com/jcondori/main/Main.java
+++ src/main/java/com/jcondori/main/Main.java
@@ -1,14 +1,23 @@
 package com.jcondori.main;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
         encryptionJcondori jcondori = new encryptionJcondori();
 
-//        System.out.println(jcondori.desencriptar("Demo"));
-        System.out.println(jcondori.encriptar("♠♥☻☺❢☺❣☻♨♠♠❢☺❣☻♠♫☺❢☺❣☻☎♠☺❢☺"));
+        System.out.println(jcondori.encriptar("Demo"));
+        System.out.println(jcondori.desencriptar("♠♥☻☺❢☺❣☻♨♠♠❢☺❣☻♠♫☺❢☺❣☻☎♠☺❢☺"));
+
+//        String name = "admin"; // String to check it's value
+//        int nameLenght = name.length(); // length of the string used for the loop
+
+//        for (int i = 0; i < nameLenght; i++) {   // while counting characters if less than the length add one
+//            char character = name.charAt(i); // start on the first character
+//            int ascii = (int) character; //convert the first character
+//            System.out.println(character + " = " + ascii); // print the character and it's value in ascii
+//        }
+
+//        System.out.println( (char)83);
+
     }
 }
